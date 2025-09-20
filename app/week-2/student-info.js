b15034d23aaee79ed37916cc1de70309a7490dd8
Aidan <aidan@tombnetwork.ca>
2025-09-19 20:55:32 -0600

    feat: week 2 assignment steps

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Aidan Hurlbut</p>
+            <Link href="https://github.com/itzaidan/cprg306-assignments">https://github.com/itzaidan/cprg306-assignments</Link>
+        </main>
+    );
+}
