43c7fae1887a0e8252f15b273fb949b67c680852
Aidan <aidan@tombnetwork.ca>
2025-09-26 22:41:31 -0600

    fix: links wrap and add week 3 link

--- app/page.js
+++ app/page.js
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col">
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">Week 2</Link>
+      <Link href="/week-3">Week 3</Link>
     </main>
   );
 }
