3206030e2f48321bcc2a996aeb13d382ad9d2f05
Aidan <aidan@tombnetwork.ca>
2025-10-03 23:32:39 -0600

    fix: main page breaks

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">Week 2</Link>
+      <br />
       <Link href="/week-3">Week 3</Link>
+      <br />
+      <Link href="/week-4">Week 4</Link>
     </main>
   );
 }
